42c9e98f53f859c6dfd5c14fcbb0f0fea2319ee5
servorx <191423123+servorx@users.noreply.github.com.>
2025-05-09 08:54:26 -0500

    9 de mayo, sigue sin estar completo

--- js/card_component.js
+++ js/card_component.js
@@ -6,7 +6,9 @@ const HEROES = [
         universo: "DC",
         año: 1939,
         descripcionResumida: "Un vigilante enmascarado que protege Gotham.",
-        descripcionCompleta: "Bruce Wayne, también conocido como Batman, es un multimillonario filántropo que combate el crimen en Gotham usando su intelecto, habilidades físicas y tecnología avanzada."
+        descripcionCompleta: "Bruce Wayne, también conocido como Batman, es un multimillonario filántropo que combate el crimen en Gotham usando su intelecto, habilidades físicas y tecnología avanzada.",
+        imagen: "./img/bat_man.jpeg",
+        imagen1: "./img/bat_man1.jpeg"
     },
     {
         nombre: "Steve Rogers",
@@ -14,7 +16,9 @@ const HEROES = [
         universo: "Marvel",
         año: 1941,
         descripcionResumida: "Un super soldado patriótico de la Segunda Guerra Mundial.",
-        descripcionCompleta: "Steve Rogers fue transformado en un supersoldado mediante un experimento durante la Segunda Guerra Mundial y desde entonces lucha por la justicia como Capitán América, símbolo de valor y liderazgo."
+        descripcionCompleta: "Steve Rogers fue transformado en un supersoldado mediante un experimento durante la Segunda Guerra Mundial y desde entonces lucha por la justicia como Capitán América, símbolo de valor y liderazgo.",
+        imagen: "./img/capitan_america.jpeg",
+        imagen1: "./img/capitan_america1.jpeg"
     },
     {
         nombre: "Barry Allen",
@@ -22,7 +26,9 @@ const HEROES = [
         universo: "DC",
         año: 1956,
         descripcionResumida: "El hombre más rápido del mundo.",
-        descripcionCompleta: "Barry Allen adquirió su velocidad sobrehumana tras un accidente en un laboratorio. Como Flash, protege Central City enfrentándose a amenazas con su velocidad y su ingenio científico."
+        descripcionCompleta: "Barry Allen adquirió su velocidad sobrehumana tras un accidente en un laboratorio. Como Flash, protege Central City enfrentándose a amenazas con su velocidad y su ingenio científico.",
+        imagen: "./img/flash.jpeg",
+        imagen1: "./img/flash1.jpeg"
     },
     {
         nombre: "Bruce Banner",
@@ -30,7 +36,9 @@ const HEROES = [
         universo: "Marvel",
         año: 1962,
         descripcionResumida: "Un científico con una fuerza descomunal cuando se enfurece.",
-        descripcionCompleta: "Bruce Banner se transforma en Hulk, una criatura de fuerza colosal, cuando se enfurece. Lucha con su dualidad entre razón y furia mientras ayuda a salvar al mundo."
+        descripcionCompleta: "Bruce Banner se transforma en Hulk, una criatura de fuerza colosal, cuando se enfurece. Lucha con su dualidad entre razón y furia mientras ayuda a salvar al mundo.",
+        imagen: "./img/hulk.jpeg",
+        imagen1: "./img/hulk1.jpeg"
     },
     {
         nombre: "Tony Stark",
@@ -38,7 +46,9 @@ const HEROES = [
         universo: "Marvel",
         año: 1963,
         descripcionResumida: "Un genio millonario con una armadura tecnológica.",
-        descripcionCompleta: "Tony Stark es un empresario brillante que diseñó una armadura de alta tecnología para salvar su vida. Como Iron Man, combate amenazas globales combinando inteligencia, innovación y carisma."
+        descripcionCompleta: "Tony Stark es un empresario brillante que diseñó una armadura de alta tecnología para salvar su vida. Como Iron Man, combate amenazas globales combinando inteligencia, innovación y carisma.",
+        imagen: "./img/iron_man.jpeg",
+        imagen1: "./img/iron_man1.jpeg"
     },
     {
         nombre: "Hal Jordan",
@@ -46,7 +56,9 @@ const HEROES = [
         universo: "DC",
         año: 1959,
         descripcionResumida: "Un piloto que empuña el anillo del poder.",
-        descripcionCompleta: "Hal Jordan es un piloto de pruebas elegido por los Guardianes del Universo para portar un anillo de poder que le permite crear construcciones con su voluntad como miembro del Cuerpo de Linternas Verdes."
+        descripcionCompleta: "Hal Jordan es un piloto de pruebas elegido por los Guardianes del Universo para portar un anillo de poder que le permite crear construcciones con su voluntad como miembro del Cuerpo de Linternas Verdes.",
+        imagen: "./img/linterna_verde.jpeg",
+        imagen1: "./img/linterna_verde1.jpeg"
     },
     {
         nombre: "Clark Kent",
@@ -54,7 +66,9 @@ const HEROES = [
         universo: "DC",
         año: 1938,
         descripcionResumida: "El último hijo de Krypton con poderes sobrehumanos.",
-        descripcionCompleta: "Clark Kent, nacido como Kal-El en el planeta Krypton, posee habilidades como vuelo, fuerza sobrehumana y visión de rayos X. Como Superman, defiende a la humanidad con un fuerte sentido de justicia."
+        descripcionCompleta: "Clark Kent, nacido como Kal-El en el planeta Krypton, posee habilidades como vuelo, fuerza sobrehumana y visión de rayos X. Como Superman, defiende a la humanidad con un fuerte sentido de justicia.",
+        imagen: "./img/super_man.jpeg",
+        imagen1: "./img/super_man1.jpeg"
     },
     {
         nombre: "Thor Odinson",
@@ -62,7 +76,9 @@ const HEROES = [
         universo: "Marvel",
         año: 1962,
         descripcionResumida: "El dios del trueno con un martillo mágico.",
-        descripcionCompleta: "Thor, hijo de Odín y príncipe de Asgard, empuña el poderoso martillo Mjolnir. Combina su herencia divina con valentía para proteger tanto la Tierra como los Nueve Reinos."
+        descripcionCompleta: "Thor, hijo de Odín y príncipe de Asgard, empuña el poderoso martillo Mjolnir. Combina su herencia divina con valentía para proteger tanto la Tierra como los Nueve Reinos.",
+        imagen: "./img/thor.jpeg",
+        imagen1: "./img/thor.jpeg"
     },
     {
         nombre: "Diana Prince",
@@ -74,85 +90,93 @@ const HEROES = [
     }
 ];
 
-// 
+// carta del heroe
 class heroCard extends HTMLElement {
-    constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
+    constructor(HEROES) {
+        super();
+        this.attachShadow({ mode: "open" });
 
-    const container = document.createElement('div');
-    container.innerHTML = /*HTML*/`
-        <section>
-            <header>
-                <slot id="imagen"></slot>
-            </header>
-            <main>
-                <slot id="nombre"></slot>
-                <slot id="nombreClave"></slot>
-                <slot id="universo"></slot>
-                <slot id="año"></slot>
-                <slot id="descripcionResumida"></slot>
-            </main>
-            <footer>
-                <button id="button-main">Ver más</button>
-            </footer>
-        </section>
-    `;
-    
+        // contenido del componente
+        // TODO: Terminar lo de los componentes, no funciona
+        const container = document.createElement('div');
+        container.innerHTML = /*HTML*/`
+            <section>
+                <header>
+                    <slot class="imagen"></slot>
+                </header>
+                <main>
+                    <slot class="nombre"></slot>
+                    <slot class="nombreClave"></slot>
+                    <slot class="universo"></slot>
+                    <slot class="año"></slot>
+                    <slot class="descripcionResumida"></slot>
+                </main>
+                <footer>
+                    <button class="button-main">Ver más</button>
+                </footer>
+            </section>
+        `;
+        
+        // contenido del style
+        const style = document.createElement('style');
+        style.textContent = /*CSS*/`
+        section {
+            border: 2px solid #888;
+            padding: 12px;
+            border-radius: 10px;
+            font-family: Arial;
+            max-width: 400px;
+            background: #fefefe;
+        }
+        header {
+            font-weight: bold;
+            font-size: 20px;
+            margin-bottom: 10px;
+            color: #003366;
+        }
+        main {
+            font-size: 16px;
+            margin-bottom: 10px;
+            color: #222;
+        }
+        footer {
+            font-size: 12px;
+            color: #555;
+            text-align: right;
+        }
+        `;
 
-    const style = document.createElement('style');
-    style.textContent = /*CSS*/`
-    section {
-        border: 2px solid #888;
-        padding: 12px;
-        border-radius: 10px;
-        font-family: Arial;
-        max-width: 400px;
-        background: #fefefe;
-    }
-    header {
-        font-weight: bold;
-        font-size: 20px;
-        margin-bottom: 10px;
-        color: #003366;
-    }
-    main {
-        font-size: 16px;
-        margin-bottom: 10px;
-        color: #222;
-    }
-    footer {
-        font-size: 12px;
-        color: #555;
-        text-align: right;
-    }
-    `;
+        // añadir contenido que se escribió al shadow DOM
+        shadow.append(style, container);
+        }
 
-    shadow.append(style, container);
-    }
-
-    connectedCallback() {
-        const imagen = this.shadowRoot.querySelector('#imagen');
-        const nombre = this.shadowRoot.querySelector('#nombre');
-        const nombreClave = this.shadowRoot.querySelector('#nombreClave');
-        const universo = this.shadowRoot.querySelector('#universo');
-        const año = this.shadowRoot.querySelector('#año');
-        const descripcionResumida = this.shadowRoot.querySelector('#descricpionResumida');
-
-        slot.addEventListener('slotchange', () => {
-            const elementos = slot.assignedElements();
-            info.textContent = `Elementos en el slot: ${elementos.length}`;
-        });
-    }
+        // TODO: terminar lo de los callbacks, no sé cómo colocar cada elemento dentro del callback
+        connectedCallback() {
+            const imagen = this.shadowRoot.querySelector('slot[class="imagen"]');
+            const nombre = this.shadowRoot.querySelector('slot[class="nombre"]');
+            const nombreClave = this.shadowRoot.querySelector('slot=[class="nombreClave"]');
+            const universo = this.shadowRoot.querySelector('slot=[class="universo"]');
+            const año = this.shadowRoot.querySelector('class=[class="año"]');
+            const descripcionResumida = this.shadowRoot.querySelector('class=["descricpionResumida"]');
+                
+            imagen.addEventListener('slotchange', () => {
+                const heroe_elemento = imagen.assignedElements();
+                info.innerHTML = `<img src="${imagen}.jpeg" alt="bat_man">`;
+            });
+        }
 }
+
+// registrar el componente commo <hero-card>
 customElements.define("hero-card", heroCard);
 
-// 
+
+// ventana con detalles
 class heroFull extends HTMLElement {
     constructor() {
     super();
     this.attachShadow({ mode: "open" });
 
+    // TODO: Terminar de arreglar los estilos
     const container = document.createElement('div');
     container.innerHTML = `
     <section>
@@ -168,6 +192,7 @@ class heroFull extends HTMLElement {
     </section>
     `;
 
+    // TODO: terminar de arreglar los estilos
     const style = document.createElement('style');
     style.textContent = `
     section {
@@ -197,6 +222,40 @@ class heroFull extends HTMLElement {
     `;
 
     shadow.append(style, container);
+    }
+    // TODO: terminar lo de los callbacks, no sé cómo colocar cada elemento dentro del callback
+    connectedCallback() {
+        const imagen = this.shadowRoot.get('slot[class="imagen"]');
+        const nombre = this.shadowRoot.querySelector('slot[class="nombre"]');
+        const nombreClave = this.shadowRoot.querySelector('slot=[class="nombreClave"]');
+        const universo = this.shadowRoot.querySelector('slot=[class="universo"]');
+        const year = this.shadowRoot.querySelector('class=[class="year"]');
+        const descripcionResumida = this.shadowRoot.querySelector('class=["descricpionResumida"]');
+            
+        imagen.addEventListener('slotchange', () => {
+            const a = imagen.assignedElements();
+            info.innerHTML = `<img src="/img/${}.jpeg" alt="bat_man">`;
+        });
+        nombre.addEventListener('slotchange', () => {
+            const a = nombre.assignedElements();
+            info.innerHTML = `<img src="/img/${}.jpeg" alt="bat_man">`;
+        });
+        nombreClave.addEventListener('slotchange', () => {
+            const a = nombreClave.assignedElements();
+            info.innerHTML = `<img src="/img/${}.jpeg" alt="bat_man">`;
+        });
+        universo.addEventListener('slotchange', () => {
+            const a = universo.assignedElements();
+            info.innerHTML = `<img src="/img/${}.jpeg" alt="bat_man">`;
+        });
+        year.addEventListener('slotchange', () => {
+            const a = nombre.assignedElements();
+            info.innerHTML = `<img src="/img/${}.jpeg" alt="bat_man">`;
+        });
+        descripcionResumida.addEventListener('slotchange', () => {
+            const a = nombre.assignedElements();
+            info.innerHTML = `<img src="/img/${}.jpeg" alt="bat_man">`;
+        });
     }
 }
 customElements.define("hero-full", heroFull);
--- js/search_bar.js
+++ js/search_bar.js
@@ -72,29 +72,137 @@ const HEROES = [
         descripcionCompleta: "Diana, princesa de las Amazonas, es una semidiosa entrenada en combate desde pequeña. Como Wonder Woman, defiende la paz, la justicia y la igualdad en el mundo humano."
     }
 ];
-const container = document.getElementById("cards");
-const input = document.getElementById("search");
 
-function renderHeroes(list) {
-    container.innerHTML = "";
-    list.forEach(hero => {
-        const card = document.createElement("hero-card");
-        card.data = hero;
-        container.appendChild(card);
-    });
-}
 
-renderHeroes(HEROES); // Mostrar todos al inicio
+// function renderHeroes(list) {
+//     container.innerHTML = "";
+//     list.forEach(hero => {
+//         const card = document.createElement("hero-card");
+//         card.data = hero;
+//         container.appendChild(card);
+//     });
+// }
 
-input.addEventListener("input", (e) => {
-    const valor = e.target.value.toLowerCase();
-    const filtrados = heroes.filter(h => h.nombreClave.toLowerCase().includes(valor));
-    console.log(`El usuario ingresó un personaje ${valor} en el buscador`);
-    renderHeroes(filtrados);
-});
+// renderHeroes(HEROES); // Mostrar todos al inicio
 
-document.body.addEventListener("show-modal", (event) => {
-    const modal = document.createElement("hero-modal");
-    modal.data = event.detail;
-    document.body.appendChild(modal);
-});
+// input.addEventListener("input", (e) => {
+//     const valor = e.target.value.toLowerCase();
+//     const filtrados = HEROES.filter(h => h.nombreClave.toLowerCase().includes(valor));
+//     console.log(`El usuario ingresó un personaje ${valor} en el buscador`);
+//     renderHeroes(filtrados);
+// });
+
+// document.body.addEventListener("show-full", (event) => {
+//     const modal = document.createElement("hero-full");
+//     modal.data = event.detail;
+//     document.body.appendChild(modal);
+// });
+
+
+// codigo de barra de busqeuda HTML y CSS
+
+// const clotheList = [
+//     "Tea Dress",
+//     "Pinafore Dress",
+//     "Swing Dress",
+//     "Shirtwaist Dress",
+//     "Peplum Dress",
+//     "Tweed Dress",
+//     "Flapper Dress",
+//     "Gingham Dress",
+//     "Puff Sleeve Blouse",
+//     "Peter Pan Collar Blouse",
+//     "Lace Blouse",
+//     "Victorian Blouse",
+//     "Gibson Girl Blouse",
+//     "Bow-Tie Blouse",
+//     "Edwardian Blouse",
+//     "Silk Blouse",
+//     "High-Waisted Trousers",
+//     "Wide-Leg Pants",
+//     "Culottes",
+//     "Pleated Skirt",
+//     "A-Line Skirt",
+//     "Circle Skirt",
+//     "Pencil Skirt",
+//     "Tartan Skirt",
+//     "Swing Coat",
+//     "Trench Coat",
+//     "Cape Coat",
+//     "Faux Fur Coat",
+//     "Bolero Jacket",
+//     "Cardigan Sweater",
+//     "Twin Set",
+//     "Cable Knit Sweater",
+//     "Beret Hat",
+//     "Cloche Hat",
+//     "Pillbox Hat",
+//     "Fascinator",
+//     "Cat Eye Sunglasses",
+//     "Round Frame Glasses",
+//     "Pearl Necklace",
+//     "Charm Bracelet",
+//     "Brooch",
+//     "Silk Scarf",
+//     "Lace Gloves",
+//     "Satin Gloves",
+//     "Oxford Shoes",
+//     "Mary Jane Shoes",
+//     "T-Strap Heels",
+//     "Kitten Heels",
+//     "Saddle Shoes",
+//     "Vintage Handbag",
+//     "Beaded Purse",
+//     "Crochet Bag",
+//     "Straw Handbag",
+//     "Bustier Top",
+//     "Corset Top",
+//     "Petticoat",
+//     "Stockings",
+//     "Seamed Tights",
+//     "Vintage Lingerie",
+//     "Ruffled Bloomers",
+//     "Apron Dress",
+//     "Modern light Clothes",
+//     "Light Dress Bless",
+//     "Midnight Edge Faux Leather Jacket",
+//     "Golden Glow Wrap Dress",
+// ]; 
+// // esta es la lista que se expresa para la barra de busqueda creando un array
+
+
+// // se obtienen referencias de resultado y la barra de busqueda a traves del query(no se que es eso)
+// const resultBox = document.querySelector(".results");
+// const inputBox = document.querySelector(".search-bar");
+
+// // se toma un array de los resultados filtrados y genera una lisa para mostrarlos, cada <li> tiene un onlick que le permite autocompletarse a si mismo 
+// const displayResults = function (result) {
+//     const resultHTML = result.map(function (clothe) {
+//         return `<li onclick="selectInput(this)">${clothe}</li>`;
+//     });
+
+//     resultBox.innerHTML = "<ul>" + resultHTML.join("") + "</ul>";
+// };
+
+// // es una funcion que sucede cuando un usuario empieza a escribir en la barra de busqeuda, convierte el texto a minmusculas para evitar problemas, si el campo de entrada esta vacio, no muestra nada, si hay texto, filtra la lista de prendas, y al final llama a displayResults para mostrar la lista filtrada
+// inputBox.onkeyup = function (e) {
+//     let result = [];
+//     const input = inputBox.value.toLowerCase();
+
+//     if (input.length === 0) {
+//         resultBox.innerHTML = "";
+//         return;
+//     }
+
+//     result = clotheList.filter((clothe) =>
+//         clothe.toLowerCase().includes(input)
+//     );
+
+//     displayResults(result);
+// };
+
+// // funcion para seleccionar una prenda, cuando se hace click, se coloca en la barra de busqueda y luego, los resultados desaparecen.
+// function selectInput(clothe) {
+//     inputBox.value = clothe.innerText;
+//     resultBox.innerHTML = "";
+// }
